263b3e34374840abf346541bd734cfd03d25ef3a
juliaalvessouza <76960666+juliaalvessouza@users.noreply.github.com>
2022-11-14 11:54:07 -0300

    cors

--- routes.py
+++ routes.py
@@ -8,6 +8,18 @@ from entities.ads import *
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080",    
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # students
 @app.get("/student/{interest}")
